f87f234bedc97ef05ae0de36dfb8b68591e09cd1
Fehmi Koray Mullaoğlu <femullao@k1m30s08.42istanbul.com.tr>
2024-11-11 19:32:39 +0300

    gnelg

---
+++ get_next_line_bonus.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int ft_strlen_gnl(char *s);
+char *ft_strjoin_gnl(char *s1, char *s2);
+int ft_strchr_gnl(char *s, char c);
+char *get_next_line(int fd);
+
+
+
+#endif
