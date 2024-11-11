26723a6f055784f235f203956a1a40b99c1ffaea
Fehmi Koray Mullaoğlu <femullao@k1m30s10.42istanbul.com.tr>
2024-11-11 20:27:00 +0300

    gnlwb

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: femullao <femullao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:32:01 by femullao          #+#    #+#             */
-/*   Updated: 2024/11/11 20:15:50 by femullao         ###   ########.fr       */
+/*   Updated: 2024/11/11 20:24:32 by femullao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,10 @@ char	*ft_strjoin_gnl(char *s1, char *s2)
 	if (!r)
 		return (NULL);
 	while (s1[i])
-		r[i++] = s1[i];
+	{
+		r[i] = s1[i];
+		i++;
+	}
 	j = 0;
 	while (s2[j])
 	{
